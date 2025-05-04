af87b34d692b6166f2498af9d3106e77a7464f43
ezmiller-su <ezrajmiller@hotmail.com>
2025-05-04 16:44:55 -0400

    5

--- code/tully_scraper.py
+++ code/tully_scraper.py
@@ -11,13 +11,13 @@ def tullyscraper(playwright: Playwright) -> None:
     page.goto("https://www.tullysgoodtimes.com/menus/")
 
     items = []
-    for title in page.query_selector_all(""):
+    for title in page.query_selector_all("h3.foodmenu__menu-section-title"):
         title_contents = title.inner_text()
-        row = title.query_selector("").query_selector("")
-        for item in row.query_selector_all(""):
+        row = title.query_selector("~ *").query_selector("~ *")
+        for item in row.query_selector_all("div.foodmenu__menu-item"):
             item_text = item.inner_text()
-            extracted_item = extract_menu_item(title_contents, item_text)
-            items.append(extracted_item.to_dict())
+            item = extract_menu_item(title_contents, item_text)
+            items.append(item.to_dict())
 
     return
     # ---------------------
